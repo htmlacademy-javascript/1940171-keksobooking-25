b4d19b2a3f65eab66006fbe43c2f94ca6f85c982
Gleb <firecrow47@gmail.com>
2022-02-20 16:30:49 +0500

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
 function getRandom(min, max) {
-  let rand;
   if (min>=max) {
-    return;}
+    return 0;}
   else {
-    rand = Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  return rand;
 }
 getRandom(1, 10);
 
 function getRandomFraction(min, max, condition) {
-  let rand;
   if (min>=max) {
-    return;}
+    return 0;}
   else {
-    rand = ((Math.random() * (max - min + 1)) + min).toFixed(condition);
+    return  ((Math.random() * (max - min + 1)) + min).toFixed(condition);
   }
-  return rand;
 }
 getRandomFraction (1, 10, 5);

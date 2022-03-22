eed860d4bb3c803ee81e2e5874b04c49e5c7a86e
Gleb <firecrow47@gmail.com>
2022-03-22 13:21:46 +0500

    Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import {createSimilarAdd} from './data.js';
 import { renderPopup } from './markup-generation.js';
-
+import './user-validation.js';
 const similarAdds = createSimilarAdd(10);
 renderPopup(similarAdds[0]);
-
